fecbbe14fd5a9e024f6608fa76fcc1c0feaa1fc3
GaneshN864 <ganeshnarayanamoorthy864@gmail.com>
2026-03-10 16:39:15 +0530

    BookInsert class Ready by ganesh

--- src/main/java/com/bookstore/dao/BookRepo.java
+++ src/main/java/com/bookstore/dao/BookRepo.java
@@ -2,6 +2,8 @@ package com.bookstore.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Optional;
 
 import com.bookstore.dto.BooksDTO;
 import com.bookstore.services.GetDataSource;
@@ -9,15 +11,17 @@ import com.bookstore.services.GetDataSource;
 public class BookRepo {
 	
 	
-	public static boolean insertBookData(BooksDTO bookData) {
-	try {
+	public static boolean insertBookData(BooksDTO bookData) throws SQLException{
 		Connection connection=(Connection) GetDataSource.getDataSource();
-		PreparedStatement preparedStatement=connection.prepareStatement("insert into books") ;
-	}catch(Exception e){
-		
-		
-		}
-	return true;
+		PreparedStatement preparedStatement=connection.prepareStatement("insert into books values(?,?,?,?,?,?,?);");
+		preparedStatement.setString(1, bookData.getId());
+		preparedStatement.setString(2, bookData.getTitle());
+		preparedStatement.setString(3, bookData.getAuthor());
+		preparedStatement.setString(4, bookData.getCategory());
+		preparedStatement.setDouble(5, Double.valueOf(bookData.getPrice()));
+		preparedStatement.setString(6, bookData.getImage());
+		preparedStatement.setString(7, bookData.getDescription());
+		return preparedStatement.execute();
 	}
 }
 
