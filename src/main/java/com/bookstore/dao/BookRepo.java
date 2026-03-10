b60f6f09b8d19cf4186412c942bcdb284f5d5719
Vastpro-5 <Vastpro-5@DESKTOP-RT9V8AK>
2026-03-10 17:02:10 +0530

    datasource factory redefined by vasu

--- src/main/java/com/bookstore/dao/BookRepo.java
+++ src/main/java/com/bookstore/dao/BookRepo.java
@@ -3,21 +3,20 @@ package com.bookstore.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
+import com.bookstore.config.GetDataSource;
 import com.bookstore.dto.BooksDTO;
-import com.bookstore.services.GetDataSource;
 
 public class BookRepo {
 	
 	
 	public static boolean insertBookData(BooksDTO bookData) {
-	try {
-		Connection connection=(Connection) GetDataSource.getDataSource();
-		PreparedStatement preparedStatement=connection.prepareStatement("insert into books") ;
-	}catch(Exception e){
-		
-		
+		try {
+			Connection connection = (Connection) GetDataSource.getDataSource();
+			PreparedStatement preparedStatement = connection.prepareStatement("insert into books");
+		} catch (Exception e) {
+
 		}
-	return true;
+		return true;
 	}
 }
 
