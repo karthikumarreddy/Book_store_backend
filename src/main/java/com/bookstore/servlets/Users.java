49deb98eef75742e83e11a83e366744fcaf9c8e7
Vigneshvvigneshv <vigneshvvigneshv25@gmail.com>
2026-03-10 17:50:33 +0530

    vicky

--- src/main/java/com/bookstore/servlets/Users.java
+++ src/main/java/com/bookstore/servlets/Users.java
@@ -5,7 +5,7 @@ import java.sql.Timestamp;
 
 import com.bookstore.dto.UsersDTO;
 import com.bookstore.services.LoginService;
-import com.bookstore.services.UsersServices;
+
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -32,20 +32,12 @@ public class Users extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-<<<<<<< HEAD
-		
+
 				response.getWriter().write(LoginService.loginValidation(request, response));
-=======
-		response.setContentType("application/json");
-		response.setCharacterEncoding("UTF-8");
-		
-				response.getWriter().write(UsersServices.validateUser(request));
->>>>>>> branch 'main' of https://github.com/karthikumarreddy/Book_store_backend.git
+
 			}
-<<<<<<< HEAD
+
 	
-=======
->>>>>>> branch 'main' of https://github.com/karthikumarreddy/Book_store_backend.git
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
