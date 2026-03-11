93606c7972a88d44dfc88dd4fc0e660b010b16f7
GaneshN864 <ganeshnarayanamoorthy864@gmail.com>
2026-03-11 10:40:00 +0530

    alter the bookrepo query by ganesh

--- src/main/java/com/bookstore/dao/BookRepo.java
+++ src/main/java/com/bookstore/dao/BookRepo.java
@@ -13,14 +13,14 @@ public class BookRepo {
 	public boolean insertBookData(BooksDTO bookData) throws SQLException, ConnectionTimeoutException {
 		
 		try(Connection connection = (Connection) ConnectionFactory.getConnectionInstance();){
-			PreparedStatement preparedStatement=connection.prepareStatement("insert into books values(?,?,?,?,?,?,?);");
-			preparedStatement.setString(1, bookData.getId());
-			preparedStatement.setString(2, bookData.getTitle());
-			preparedStatement.setString(3, bookData.getAuthor());
-			preparedStatement.setString(4, bookData.getCategory());
-			preparedStatement.setDouble(5, Double.valueOf(bookData.getPrice()));
-			preparedStatement.setString(6, bookData.getImage());
-			preparedStatement.setString(7, bookData.getDescription());
+			PreparedStatement preparedStatement = connection.prepareStatement(
+					"insert into books (title,author,category,price,image,description) values(?,?,?,?,?,?);");
+			preparedStatement.setString(1, bookData.getTitle());
+			preparedStatement.setString(2, bookData.getAuthor());
+			preparedStatement.setString(3, bookData.getCategory());
+			preparedStatement.setDouble(4, Double.valueOf(bookData.getPrice()));
+			preparedStatement.setString(5, bookData.getImage());
+			preparedStatement.setString(6, bookData.getDescription());
 			int affectedRows = preparedStatement.executeUpdate();
 			if (affectedRows > 0) {
 				return true;
